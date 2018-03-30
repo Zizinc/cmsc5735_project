20c82b1ab13ecbc54be46512206f90660ee3e166
Tuxin Zhang <tuxinzhang@Tuxins-MacBook-Pro.local>
2018-03-30 22:13:08 +0800

    View

--- web/app.py
+++ web/app.py
@@ -9,20 +9,28 @@ main = Blueprint('main', __name__)
 @main.route('/dashboard')
 def dashboard_page():
 	#distinctBusinessCityDf = engine.get_spark().sql("SELECT distinct city FROM yelp_business ORDER BY city")
-	#city = distinctBusinessCityDf.collect()[0]
-	return render_template('dashboard.html')
+	distinctBusinessCityDf = engine.get_spark().sql("SELECT distinct name,city,review_count,address FROM yelp_business WHERE review_count > 1000 ORDER BY review_count desc ")
+	#Select those cities with review_count > 200
+	city = distinctBusinessCityDf.collect()
+
+	UserInfo = engine.get_spark().sql("SELECT name,review_count,yelping_since,useful,average_stars FROM yelp_user WHERE review_count > 1000 ORDER BY review_count desc ")
+	#Select those users with high review count
+
+	user = UserInfo.collect()
+
+	return render_template('dashboard.html', city=city,user=user)
 
 @main.route('/ajax_data', methods=['GET'])
 def handle_ajax_data():
 	return ajax_data_handler.handle_ajax_data(request.args.get("dataName"))
-	
+
 def create_app(spark_engine):
 	global engine
 	global ajax_data_handler
-	
+
 	engine	= spark_engine
 	ajax_data_handler = AjaxDataHandler(spark_engine)
-	
+
 	app = Flask(__name__)
 	app.register_blueprint(main)
 	return app
@@ -31,4 +39,4 @@ if __name__ == '__main__':
 	# run without spark engine
 	# for test and debug use
 	spark_engine = None
-	create_app(spark_engine).run(debug=True, host='0.0.0.0')	
+	create_app(spark_engine).run(debug=True, host='0.0.0.0')
